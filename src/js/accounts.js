8fa7d2c2c66c67a5ab0a85c4d9ab7442944589ac
TutkuEroglu <rrandqua@gmail.com>
2022-12-24 05:45:58 +0300

    project completed

---
+++ src/js/accounts.js
@@ -0,0 +1,32 @@
+export const accounts = [
+  {
+    owner: "Tutku Eroglu",
+    movements: [200, -450, 1050, 3000, -250, -730, 7000, 1300],
+    interestRate: 1.2,
+    pin: 123,
+  },
+  {
+    owner: "Deniz Bahçe",
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.2,
+    pin: 111,
+  },
+  {
+    owner: "Murat Yılmaz",
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 222,
+  },
+  {
+    owner: "Ayşe Hayat",
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1.2,
+    pin: 333,
+  },
+  {
+    owner: "Asya Adıgüzel",
+    movements: [-130, -70, 555],
+    interestRate: 1.2,
+    pin: 444,
+  },
+]
